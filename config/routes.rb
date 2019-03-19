2784b55ca3bda0c5064ab150a1a39cd4ad41db65
louieogrady <l.ogrady@gmail.com>
2019-03-19 10:19:38 +0000

    fin

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :students # only: [:new, :create, :show, :edit, :update]
+
+  get 'school_classes/new', to: 'school_classes#new', as: :new_school_class
+  resources :school_classes, except: [:new]
+
 end
